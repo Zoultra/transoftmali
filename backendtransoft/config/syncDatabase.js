d1c03c85569ccde580e8e5e2158c41b4daf3b42f
Zoumana <traorezoul7@gmail.com>
2025-03-28 03:54:33 +0000

    Correction2 de l'ordre de la creation des tables

--- backendtransoft/config/syncDatabase.js
+++ backendtransoft/config/syncDatabase.js
@@ -4,8 +4,10 @@ import '../models/associations.js'; // Chargez les associations
 
 const syncDatabase = async () => {
   try {
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ force: true });
     console.log("Les tables ont été synchronisées avec succès !");
+    await Company.sync(); // D'abord la table parent
+    await Route.sync(); // Ensuite la table enfant qui dépend de Companies
   } catch (error) {
     console.error("Erreur lors de la synchronisation :", error);
   }
