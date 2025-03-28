1238809179d30d42f012bb8aaf43d9e26adb8b84
Zoumana <traorezoul7@gmail.com>
2025-03-28 15:51:35 +0000

    Correction7 de l'ordre de la creation des tables

--- backendtransoft/config/syncDatabase.js
+++ backendtransoft/config/syncDatabase.js
@@ -8,6 +8,8 @@ const syncDatabase = async () => {
     console.log("Les tables ont été synchronisées avec succès !");
     await Company.sync(); // D'abord la table parent
     await Route.sync(); // Ensuite la table enfant qui dépend de Companies
+    await Reservations.sync();  // D'abord, créer la table Reservations
+    await Passenger.sync();  // Ensuite, créer la table Passenger qui y fait référence
   } catch (error) {
     console.error("Erreur lors de la synchronisation :", error);
   }
