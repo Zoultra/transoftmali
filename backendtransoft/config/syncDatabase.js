e23176f8d119b97e887b33f2af60c30fcd3a98b3
unknown <traorezoul7@gmail.com>
2026-01-12 17:40:41 +0000

    Update env for db

--- backendtransoft/config/syncDatabase.js
+++ backendtransoft/config/syncDatabase.js
@@ -1,20 +1,16 @@
- // syncDatabase.js
 import sequelize from './database.js';
-import '../models/associations.js'; // Chargez les associations
+import '../models/associations.js';
 
 const syncDatabase = async () => {
   try {
-    await sequelize.sync({ alter: true });
-    console.log("Les tables ont été synchronisées avec succès !");
-     // Synchroniser en respectant l'ordre des dépendances
-     await Company.sync(); // D'abord la table parent
-     await Route.sync(); // Ensuite la table enfant qui dépend de Companies
-     await Reservations.sync(); // Créer la table parente en premier
-     await Passenger.sync(); // Ensuite, créer la table qui dépend d'elle
-     await sequelize.sync({ force: true });
-    console.log("Les tables ont été synchronisées avec succès !");
+    await sequelize.authenticate();
+    console.log('✅ Connexion à la base réussie');
+
+    await sequelize.sync({ alter: true }); // ✅ SAFE POUR PROD
+    console.log('✅ Tables synchronisées avec succès');
+
   } catch (error) {
-    console.error("Erreur lors de la synchronisation :", error);
+    console.error('❌ Erreur de synchronisation :', error);
   }
 };
 
